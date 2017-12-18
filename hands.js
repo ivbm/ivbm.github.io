7e40f74efd404e222d4a62423665e17021a7dcb3
ivbm <33903935+ivbm@users.noreply.github.com>
2017-12-18 12:13:00 +0100

    Update hands.js

--- hands.js
+++ hands.js
@@ -27,21 +27,10 @@ function init() {
             x_array = this_hand.slice(0, this_hand.length/2);
             y_array = this_hand.slice(this_hand.length/2, this_hand.length);
             zipped_xy = d3.zip(x_array, y_array);
-
-
-            /*console.log("see here mate:");
-            console.log(this_hand);
-            console.log(this_hand.length);
-            console.log(x_array.length);
-            console.log(x_array);
-            console.log(y_array.length);
-            console.log(y_array);*/
-
-
-
+            
             var xScale = d3.scaleLinear()
                 .domain([0,
-                    d3.max(zipped_xy,  //may need to check another max, not sure, but it depends on input below
+                    d3.max(zipped_xy,
                         function (d) {
                             return (d)[0];
                         })])
@@ -106,27 +95,6 @@ function init() {
                 d3.selectAll("path.line").remove();
 
             });
-
-
-
-            /*d3.select('#hand')
-                .selectAll('circle')
-                .data(zipped_xy)
-                .enter()
-                .append('circle')
-                .attr('r', '3px')
-                .attr('cx', function (d) {
-
-                    return '' + xScale(d[0]) + 'px';
-
-                })
-                .attr('cy', function (d) {
-                    return '' + yScale(d[1]) + 'px';
-                })*/
-
-
-
-
         });
     }
 
@@ -181,6 +149,8 @@ function init() {
             .selectAll('circle')
             .data(pca_data);
 
+
+
         pca_circles.enter()
             .append('circle')
             .attr('r', '4px')
@@ -199,29 +169,10 @@ function init() {
                 d3.select("body")
                     .select("#handInfo")
                     .text("" + i);
-
-
+                d3.select('#pca').selectAll('circle').attr('fill','black');
+                d3.select(this).attr('fill','red');
 
 
             });
-
-
-        /*d3.select('#pca')
-            .append('g')
-            .call(d3.axisBottom(pca_xScale));
-        d3.select('#pca')
-            .append('g')
-            .call(d3.axisRight(pca_yScale));*/
-
-
-
-
-
-
     });
-
-
-
-
-
 }
