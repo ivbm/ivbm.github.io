dced451e231dda3220a073a619209343ed22fdca
Soren47 <ssp_47@yahoo.dk>
2018-01-02 16:59:55 +0100

    An example working. Just not for assignment data...

--- ass5/ass5.js
+++ ass5/ass5.js
@@ -3,5 +3,41 @@ d3.select(window).on('load', init);
 function init() {
 
 
+    //Load in GeoJSON data
+    //d3.json("sfpd_districts.json", function(json) {
+    d3.json("oceans.json", function(json) {
+
+
+        var svg = d3.select('#map');
+
+        //Width and height
+        var w = 500;
+        var h = 500;
+
+        //Define map projection
+        var projection = d3.geoMercator()
+            .translate([w/2, h/2])
+            .scale([w * 0.16]);
+
+        //Define path generator
+        var path = d3.geoPath()
+            .projection(projection);
+
+
+
+        //Bind data and create one path per GeoJSON feature
+        svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", "steelblue");
+
+    });
+
+
+
+
+
 
 }
