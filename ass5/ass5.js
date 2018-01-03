be8fec5a0d0147b78283fc4330d87a9ee9dd46e7
Soren47 <ssp_47@yahoo.dk>
2018-01-03 15:25:17 +0100

    Added some colors depending on category

--- ass5/ass5.js
+++ ass5/ass5.js
@@ -17,7 +17,7 @@ function init() {
                 .center([-122.433701, 37.767683])
                 .scale(170000)
                 //.translate([width / 1.95, height / 1.65]);
-                .translate([w/2, h/2])
+                .translate([w/2, h/2]);
             //.scale([w * 0.16]);
 
             //path
@@ -34,17 +34,44 @@ function init() {
 
             //var elem = crimes.features[0]
 
-            var categToNumber = new Map()
+            var categToNumber = new Map();
+
+            var all_categories = new Set();
+
+            var violent_cats = new Set(["ASSAULT",
+                                "ROBBERY",
+                                "SEX OFFENSES FORCIBLE",
+                                "KIDNAPPING",
+                                "ARSON",
+                                "EXTORTION",
+                                "SUICIDE"]);
+
             for(var i =0; i<crimes.features.length;i++)
             {
-                var elem = crimes.features[i].properties.Category
+                var elem = crimes.features[i].properties.Category;
+
+                /*console.log("###############");
+                console.log(elem);
+                console.log("###############");*/
+
+                all_categories.add(elem);
+
                 if(!categToNumber.has(elem))
                 {
                     categToNumber.set(elem, categToNumber.size)
                 }
             }
 
-            console.log(categToNumber)
+            console.log(categToNumber);
+            console.log(all_categories);
+            console.log(violent_cats);
+
+
+
+
+
+
+
             svg.selectAll("circle")
                 .data(crimes.features)
                 .enter()
@@ -56,8 +83,16 @@ function init() {
                     return projection(d.geometry.coordinates)[1]
                 })
                 .attr("r",1)
-                .attr("fill", function(){
-                    return
+                .attr("fill", function(d){
+
+                    if (violent_cats.has(d.properties.Category)) {
+                        return "red";
+                    } else if (d.properties.Category === "PROSTITUTION") {
+                        return "orange";
+                    } else {
+                        return "black";
+                    }
+
                 })
 
             //console.log(projection(elem.geometry.coordinates));
